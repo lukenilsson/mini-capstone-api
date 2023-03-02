d45f48c56bf4a6136e9fcf9e130c44350bdb42fe
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-02 14:55:49 -0500

    used POST to add new entry

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,13 +9,13 @@ class ProductsController < ApplicationController
     render :show
   end
 
-  # def product2
-  #   @product = Product.second
-  #   render template: "products/show"
-  # end
-
-  # def product3
-  #   @product = Product.third
-  #   render template: "products/show"
-  # end
+  def create
+    @product = Product.create(
+      name: "Furby",
+      image_url: "https://upload.wikimedia.org/wikipedia/en/7/70/Furby_picture.jpg",
+      price: 25,
+      description: "Furby is an American electronic robotic toy that was originally released in 1998 by Tiger Electronics. It resembles a hamster or owllike creature and went through a period of being a 'must-have' toy following its holiday season launch, with continual sales until 2000.",
+    )
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # get "/product1/:id", controller: "products", action: "product1"
   get "/products/:id" => "products#show"
   # get "/product2", controller: "products", action: "product2"
-
+  post "/products" => "products#create"
   # get "/product3", controller: "products", action: "product3"
 end
