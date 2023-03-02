3e73d3e0d12f6ba92690f30ae7d57a60818a1a84
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-02 16:11:46 -0500

    used params and body params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,12 +10,23 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.create(
-      name: "Furby",
-      image_url: "https://upload.wikimedia.org/wikipedia/en/7/70/Furby_picture.jpg",
-      price: 25,
-      description: "Furby is an American electronic robotic toy that was originally released in 1998 by Tiger Electronics. It resembles a hamster or owllike creature and went through a period of being a 'must-have' toy following its holiday season launch, with continual sales until 2000.",
+    @product = Product.new(
+      name: params[:name],
+      image_url: params[:image_url],
+      price: params[:price],
+      description: params[:description],
     )
+    @product.save
     render :show
   end
+
+  # def create
+  #   @product = Product.create(
+  #     name: "Furby",
+  #     image_url: "https://upload.wikimedia.org/wikipedia/en/7/70/Furby_picture.jpg",
+  #     price: 25,
+  #     description: "Furby is an American electronic robotic toy that was originally released in 1998 by Tiger Electronics. It resembles a hamster or owllike creature and went through a period of being a 'must-have' toy following its holiday season launch, with continual sales until 2000.",
+  #   )
+  #   render :show
+  # end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,19 +1,9 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "index" do
-    get "/products.json"
-    assert_response 200
-
-    data = JSON.parse(response.body)
-    assert_equal Product.count, data.length
-  end
-
-  test "show" do
-    get "/products/#{Product.first.id}.json"
-    assert_response 200
-
-    data = JSON.parse(response.body)
-    assert_equal ["id", "name", "image_url", "price", "description"], data.keys
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description" }
+    end
   end
 end
