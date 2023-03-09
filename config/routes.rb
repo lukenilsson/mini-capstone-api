ba347e514854c5e4d56e89945650b1d1134566ab
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-09 14:10:02 -0500

    Add users create action (signup)

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  # get "/products", controller: "products", action: "index"
   get "/products" => "products#index"
-  # get "/product1/:id", controller: "products", action: "product1"
   get "/products/:id" => "products#show"
-  # get "/product2", controller: "products", action: "product2"
   post "/products" => "products#create"
-  # get "/product3", controller: "products", action: "product3"
-
   patch "products/:id" => "products#update"
-
   delete "/products/:id" => "products#destroy"
-
   get "/suppliers" => "suppliers#index"
-
   get "/suppliers/:id" => "suppliers#show"
+
+  post "/users" => "users#create"
 end
