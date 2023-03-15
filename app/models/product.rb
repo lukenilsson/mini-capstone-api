1bb2c26622ded2c55a0046da4e4fbfcc69bd4625
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-15 13:35:35 -0400

    adding associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :supplier
-
   has_many :images
   has_many :orders
+  has_many :category_products
+  has_many :category, through: :category_products
 
   def is_discounted?
     if price <= 10
