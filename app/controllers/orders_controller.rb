86d9ae22ab1aab01bbfdfc821513bcce06c6690c
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-17 16:20:17 -0400

    adjusting orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,12 +4,13 @@ class OrdersController < ApplicationController
   def create
     if current_user
       product = Product.find_by(id: params[:product_id])
+      @order = CartedProduct.where(status: "carted")
       calculated_subtotal = product.price * params[:quantity].to_i
       calculated_tax = calculated_subtotal * 0.09
       calculated_total = calculated_subtotal + calculated_tax
       @order = Order.new(
         user_id: current_user.id,
-        product_id: params[:product_id],
+        product_id: carted_products,
         quantity: params[:quantity],
         subtotal: calculated_subtotal,
         tax: calculated_tax,
@@ -36,3 +37,11 @@ class OrdersController < ApplicationController
     render :show
   end
 end
+
+# def index
+#   if current_user
+#     render json: @carted_products.as_json
+#   else
+#     render json: [], status: :unauthorized
+#   end
+# end
